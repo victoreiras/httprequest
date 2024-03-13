0c1b28a81fddd96bf9600486d0dc84c039a54331
Victor Eiras <victor.eiras@abuzitos.com.br>
2024-03-13 11:39:09 -0300

    Obtendo valores do header

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace httprequest;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsuarioController : ControllerBase
+{
+    private readonly IHttpContextAccessor _contextAccessor;
+
+    public UsuarioController(IHttpContextAccessor contextAccessor)
+    {
+        _contextAccessor = contextAccessor;
+    }
+
+    [HttpPost("cadastrarUsuarios")]
+    public IActionResult CadastrarUsuarios(Usuario usuario)
+    {        
+        ObterEmpresaIdPeloHeader(_contextAccessor.HttpContext);
+        return Ok();
+    }
+
+    private void ObterEmpresaIdPeloHeader(HttpContext? context)
+    {
+        if(context is not null)
+        {
+            var header = context.Request.Headers.Where(q => q.Key == "EmpresaId").FirstOrDefault();
+
+            int empresaId = 0;
+
+            if(header.Key is not null)
+                empresaId = int.Parse(header.Value.ToString());
+        }
+    }
+}
---
+++ Model/Usuario.cs
@@ -0,0 +1,8 @@
+﻿namespace httprequest;
+
+public class Usuario
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public int EmpresaId { get; set; }
+}
